a498c4796835eef712bcfaae45220d5d719ded6c
Pramod1200 <122000763+Pramod1200@users.noreply.github.com>
2023-01-23 11:44:30 +0530

    Add files via upload

---
+++ NATUU.CPP
@@ -0,0 +1,68 @@
+#define BLYNK_TEMPLATE_ID "TMPLipn8xPYa"
+#define BLYNK_DEVICE_NAME "IRRI"
+#define BLYNK_AUTH_TOKEN "_7cP-YUWMYcS5Lz5UDL3zn5j5tgsZrld"
+#define BLYNK_PRINT Serial
+#define motor D0
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "Nayuu...!";
+char pass[] = "bhikari2";
+const int AirValue = 620;   //you need to replace this value with Value_1
+const int WaterValue = 310;  //you need to replace this value with Value_2
+int soilMoistureValue = 0;
+int soilmoisturepercent=0;
+int sensor = A0;
+BlynkTimer timer;
+void myTimerEvent()
+{
+   Blynk.virtualWrite(V0,digitalRead(motor));
+   Blynk.virtualWrite(V1,soilmoisturepercent);
+}
+void setup()
+{
+Serial.begin(9600); 
+
+
+
+pinMode(motor,OUTPUT);
+Blynk.begin(auth, ssid, pass);
+  timer.setInterval(1000L, myTimerEvent);
+
+}
+void loop()  {
+  Blynk.run();
+  timer.run();
+soilMoistureValue = analogRead(sensor);  //put Sensor insert into soil
+Serial.print("sensor value");
+Serial.println(soilMoistureValue);
+soilmoisturepercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
+if(soilmoisturepercent >= 100)
+{
+  Serial.print("moisture is ");
+  Serial.println("100 %");
+}
+else if(soilmoisturepercent <=0)
+{
+  Serial.print("moisture is ");
+  Serial.println("0 %");
+}
+else if(soilmoisturepercent >0 && soilmoisturepercent < 100)
+{
+  Serial.print("moisture is ");
+  Serial.print(soilmoisturepercent);
+  Serial.println("%");
+  
+}
+if(soilmoisturepercent <= 30)
+{
+  digitalWrite(motor,HIGH);
+  Serial.println("MOTOR IS ONN");
+  }
+  if(soilmoisturepercent >= 80)
+  {
+    digitalWrite(motor,LOW);
+    Serial.println("MOTOR IS OFF");
+  }
+  delay(1000);
+}
